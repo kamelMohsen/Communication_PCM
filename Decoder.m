0b2eb634ad70d8ddf587332cbaeedc5cdb6732da
KamelMohsen <kamelmohsenkamel@gmail.com>
2020-05-20 15:55:58 +0200

    Done Missing Quantizer

---
+++ Decoder.m
@@ -0,0 +1,51 @@
+function [decoded]=Decoder(signal,peak,levels,encoderAmplitude,mu,encoderType,quantizerType,ts)
+
+
+
+step=(2*encoderAmplitude)/levels; 
+bits=log2(levels);
+
+signal=signal./encoderAmplitude;
+bitsize=(length(signal))/bits;
+
+c=1;
+decoded=zeros(0,1);
+
+if (encoderType==1)
+   for i=1:1:length(signal)
+       if(signal(i)==-1)
+           signal(i)=0;
+       end
+   end
+end
+
+count=1;
+Temp=zeros(0,1);
+if (encoderType==2)
+   for i=1:2:(length(signal))
+       if(signal(i)==1 && signal(i+1)==-1)
+           Temp(count)=1;
+           count=count+1;
+       elseif(signal(i)==-1 && signal(i+1)==1)
+           Temp(count)=0;
+           count=count+1;
+       end
+   end
+    signal=Temp;
+end
+
+for i=1:bits:length(signal)
+    period=signal(i:times(c,bits));
+    pAmp=bi2de(period,2,'left-msb');
+    Amp=(pAmp*step)-((2^(bits-1)-1)*step)-(step/2);
+    decoded(c)=Amp;
+    c=c+1;
+   
+end
+ 
+if(quantizerType == 1)
+    Amp=decoded.*log(1+mu)./peak;
+    decoded=(peak/mu)*(exp(abs(Amp))-1).*sign(Amp);
+end
+
+end
---
+++ Sampler.m
@@ -0,0 +1,61 @@
+function [signal,sampledSignal,peak,fs,f,tSampled,tOriginal,numberOfCycles] = Sampler()
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%Entering Inputs%%%%%%%%%%%%%%%%%%%%%%%%%%%
+fprintf('<strong>Entering Sampler inputs</strong>\n');
+
+%% Entering the signal as an equation but has to enter it in matlab format 
+%% and use f for Fm and use for t equation's time  
+disp('The signal function should be in matlab format.');
+disp('i.e: 5*cos(2*pi*f*t)')
+inputFunction=input('Enter the signal function m(t): ','s');
+
+%% Enterig the signal frequency Fm
+f=input('Enter the signal frequncy: ');
+
+%% Entering the sampling Frequency
+fs=input('Enter the sampling frequncy: ');
+
+%% Entering the number of cycles 
+numberOfCycles=10;
+
+
+%% Calculating original signal
+Timestep=1/(numberOfCycles*fs);
+t=0:Timestep:numberOfCycles*1/f;
+tOriginal = t;
+catched = 1;
+while catched == 1
+	try
+		signal=eval(inputFunction);
+		catched = 0;
+	catch
+		inputFunction=input('WRONG function format please renter it: ','s');;
+	end
+end
+
+peak=max(signal);
+
+%% Plotting original signal
+figure('Name', 'Sampler');
+plot(t,signal,'LineWidth',1, 'Color', 'k', 'DisplayName', 'Signal');
+hold on; 
+
+%% Calculating sampled signal
+t=0:1/fs:numberOfCycles*1/f;
+tSampled = t;
+sampledSignal = eval(inputFunction);
+
+%% plotig the sampled signal and adding info
+stem(tSampled,sampledSignal,'o','filled', 'LineWidth',1.5, 'Color', 'm', 'DisplayName', 'Sampled signal'); 
+caption = strcat('Sampling of: ',inputFunction);
+title(caption); 
+xlabel('Time(s)'); 
+ylabel('Amplitude');
+legend('Signal','Sampled signal'); 
+legend('show')
+grid
+
+
+end
+
